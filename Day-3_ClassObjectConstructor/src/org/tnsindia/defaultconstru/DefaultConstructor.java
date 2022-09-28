eda754fc09b9eab85fea6b93abf25bdbf471aa04
DeLL <DeLL@DESKTOP-PV2MSDR>
2022-09-29 00:47:48 +0530

    third commit

---
+++ Day-3_ClassObjectConstructor/src/org/tnsindia/defaultconstru/DefaultConstructor.java
@@ -0,0 +1,33 @@
+package org.tnsindia.defaultconstru;
+
+//class definition
+class Car
+{
+	//variable
+	public int speed;
+	//method
+	void display()
+	{
+		System.out.println("The speed is: "+speed+"km/hr");
+	}
+	//default constructor
+	//Constructor is used to initialize the value of the variables
+	Car()
+	{
+		speed=50;
+	}
+}
+
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		
+		//object creation
+				Car obj=new Car();
+				//obj.speed=50;
+				//calling the method of car class
+				obj.display();
+
+	}
+
+}
