3cb3cacfa4d313bcabc2618b2677410a8fe413d4
DeLL <DeLL@DESKTOP-PV2MSDR>
2022-09-28 23:33:10 +0530

    first1 commit

---
+++ Day-1_InputFromUser/src/org/tnsindia/basic/User1.java
@@ -0,0 +1,14 @@
+package org.tnsindia.basic;
+
+public class User1 {
+
+	public static void main(String[] args) {
+		//input from user
+		int a=10;
+		String s="nutan";
+		System.out.println(a);
+		System.out.println(s);
+
+	}
+
+}
