9c4ec80f80d7380927392d63dbac59e647b29247
DeLL <DeLL@DESKTOP-PV2MSDR>
2022-10-20 14:41:16 +0530

    First Commit!!!!!

---
+++ Day-11_Collections/src/org/tnsindia/camparatordemo/Employee.java
@@ -0,0 +1,25 @@
+package org.tnsindia.camparatordemo;
+
+public class Employee {
+	private int age;
+	private String name;
+	//setters and getters method
+	public int getAge1() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
