717ac98c6d2f4f1b2ddd94ef679a9d2999a4c453
DeLL <DeLL@DESKTOP-PV2MSDR>
2022-10-20 14:52:26 +0530

    First Commit

---
+++ Day-12_Date-time_API/src/org/tnsindia/datetime/TreeSetDemo.java
@@ -0,0 +1,18 @@
+package org.tnsindia.datetime;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		SortedSet<String> s=new TreeSet<>();
+		s.add("nutan");
+		s.add("Shubham");
+		s.add("ravina");
+		s.add("Rutuja");
+		System.out.println("The elements in the TreeSet are: "+s);
+
+	}
+
+}
