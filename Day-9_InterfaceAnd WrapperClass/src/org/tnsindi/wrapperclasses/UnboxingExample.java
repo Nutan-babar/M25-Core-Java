50f756a71adf4f7350fa18ca1a8d0197c0c31409
DeLL <DeLL@DESKTOP-PV2MSDR>
2022-10-20 11:37:29 +0530

    First Commit!!!

---
+++ Day-9_InterfaceAnd WrapperClass/src/org/tnsindi/wrapperclasses/UnboxingExample.java
@@ -0,0 +1,14 @@
+package org.tnsindi.wrapperclasses;
+
+public class UnboxingExample {
+	
+	public static void main(String[] args) {
+		 Integer a=15;
+		 //unboxing
+		 int x=a;
+		 
+		 System.out.println(x+ " "+a);
+
+	}
+
+}
